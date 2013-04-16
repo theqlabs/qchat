61d0e19aeef2521271cb976b2bcf79d7c670abc5
Andrew Righter <q@theqlabs.com>
2013-04-16 14:02:41 -0400

    Adding Xcode project tree to repository.

---
+++ qchat/qchat/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  qchat
+//
+//  Created by q on 4/16/13.
+//  Copyright (c) 2013 theqlabs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
