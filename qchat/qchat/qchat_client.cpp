817dc72d05d5c9c09548a44fd656268fbd799882
Michael Collis <michael.l.collis@gmail.com>
2013-04-19 15:59:27 -0400

    Added chat setup and IP discovery framework

--- qchat/qchat/qchat_client.cpp
+++ qchat/qchat/qchat_client.cpp
@@ -7,50 +7,101 @@
 //
 //  @authors:
 //  Andrew Righter - @theqlabs (GitHub/Twitter)
-//  Michael Collis
+//  Michael Collis - mcollis@cis.upenn.edu 
 //
 
 #include <iostream>
-#include "qchat.h"
 #include <rpc/rpc.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "qchat.h"
 
 using namespace std;
 
 // removed const from arg vector to satisfy throwing argv[2] into char *host;
 int main(int argc, char * argv[]) {
     
-    CLIENT *clnt;
-    void *result_1;
-    char * printmessage_1_arg;
-    char *host;
+  CLIENT *clnt;
+  void *result_1;
+  char * printmessage_1_arg;
+  char *hostName = (char*) malloc(INET_ADDRSTRLEN);
 
-    string hostName = "";
-    string usrName = "";
-    string myMessage = "";
-    
-    if (argc != 3) {
-        cout << "Usage ./qchat nickname server" << endl;
-        return 1;
+  if (hostName == nullptr) {
+    cout << "Chat hostname memory allocation failed" << endl;
+    return 1;
+  }
+  
+  string usrName = "";
+  
+  if (argc > 3 || argc < 2) {
+    cout << "Usage ./dchat nickname [host server IP:PORT]" << endl;
+    return 1;
+  } else if (argc == 3) {
+    // Joining an existing chat
+    hostName = argv[2];
+    cout << "" << endl;
+  } else {
+    // Creating a new chat
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+      cout << "Error discovering local IP address" << endl;
+      return 1;
     }
     
-    host = argv[2];
-        
-    // create a CLIENT handle
-    clnt = clnt_create(host, QCHAT, QCHATVERS, "udp");
+    const char* openDnsAddr = "208.67.222.222";
+    uint16_t dnsPort = 53;
+    struct sockaddr_in socketadd;
+    memset(&socketadd, 0, sizeof(socketadd));
+    socketadd.sin_family = AF_INET;
+    socketadd.sin_addr.s_addr = inet_addr(openDnsAddr);
+    socketadd.sin_port = htons(dnsPort);
     
-    // if connection doesn't succeed
-    if (clnt == NULL) {
-        clnt_pcreateerror(host);
-        return 1;
+    int err = connect(sock, (const sockaddr*) &socketadd, sizeof(socketadd));
+    if (sock == -1) {
+      cout << "Error discovering local IP address" << endl;
+      return 1;
+    }
+    sockaddr_in sockname;
+    socklen_t socknamelen = sizeof(sockname);
+    err = getsockname(sock, (sockaddr*) &sockname, &socknamelen);
+    if (sock == -1) {
+      cout << "Error discovering local IP address" << endl;
+      return 1;
     }
     
-    // run PRINTMESSAGE function from qchat_server.cpp
-    result_1 = printmessage_1(&printmessage_1_arg, clnt);
-    
-    cout << "Welcome " << usrName << " to qchat on " << hostName << endl;
-    cout << usrName << ": ";
-    cin >> myMessage;
+    const char* p = inet_ntop(AF_INET, &sockname.sin_addr, hostName, INET_ADDRSTRLEN);
+    if (p == NULL) {
+      cout << "Error discovering local IP address" << endl;
+      return 1;
+    }
+  }
+  
+  string myMessage = "";
         
-    return 0;
+  // create a CLIENT handle
+  clnt = clnt_create(hostName, QCHAT, QCHATVERS, (char*)"udp");
+    
+  // if connection doesn't succeed
+  if (clnt == NULL) {
+      clnt_pcreateerror(hostName);
+    cout << "Sorry, no chat is active on " << hostName << ", try again later. " << endl;
+      return 1;
+  }
+  
+  
+  
+  // run PRINTMESSAGE function from qchat_server.cpp
+  result_1 = printmessage_1(&printmessage_1_arg, clnt);
+    
+  cout << "Welcome " << usrName << " to qchat on " << hostName << endl;
+  cout << usrName << ": ";
+  cin >> myMessage;
+  
+  if(hostName != nullptr) {
+    free(hostName);
+  }
+  return 0;
     
 }
