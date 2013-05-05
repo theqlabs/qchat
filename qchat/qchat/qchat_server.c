bffc39b0f499fd7135bba15f4817e1fed49cbd4d
Andrew Righter <q@theqlabs.com>
2013-05-05 01:51:10 -0400

    debug code for exit RPC.

--- qchat/qchat/qchat_server.c
+++ qchat/qchat/qchat_server.c
@@ -292,6 +292,8 @@ int *exit_1_svc(uname *user, struct svc_req *rqstp) {
 		init_data_structures();
 	}
 
+	printf("DOES ANYONE SEE THIS?");
+
 	for (i=0; i<clients->clientlist.clientlist_len; i++) {
 		if(strcmp(user, clients->clientlist.clientlist_val[i].userName) == 0) {
 			if(i<(clients->clientlist.clientlist_len-1)) {
